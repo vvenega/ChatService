c6d438a7417885e526ed87e17a18438917d449b1
victorv3@illinois.edu <victorv3@illinois.edu>
2021-06-18 19:33:25 -0500

    New Class Chatter

---
+++ src/main/java/com/example/demo/ChatterBean.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class ChatterBean {
+	
+	//private String name;
+	private String username;
+	private String group;
+	
+	/*public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}*/
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	
+
+}
